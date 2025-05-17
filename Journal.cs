97fd6e853bed54572c4e7794f3997ebeb25b0605
estherokondev <estherakpanokon@gmail.com>
2025-05-17 21:52:08 +0100

    Journal.cs
    journal design

---
+++ Journal.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Journal
+{
+    private List<Entry> _entries = new List<Entry>();
+
+    private List<string> _prompts = new List<string>
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
+    };
+
+    public void AddEntry()
+    {
+        Random rand = new Random();
+        string prompt = _prompts[rand.Next(_prompts.Count)];
+        Console.WriteLine($"Prompt: {prompt}");
+        Console.Write("Your response: ");
+        string response = Console.ReadLine();
+        Entry entry = new Entry(prompt, response);
+        _entries.Add(entry);
+    }
+
+    public void DisplayJournal()
+    {
+        foreach (Entry entry in _entries)
+        {
+            Console.WriteLine(entry);
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter writer = new StreamWriter(filename))
+        {
+            foreach (Entry entry in _entries)
+            {
+                writer.WriteLine(entry.ToFileFormat());
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        _entries.Clear();
+        string[] lines = File.ReadAllLines(filename);
+        foreach (string line in lines)
+        {
+            _entries.Add(Entry.FromFileFormat(line));
+        }
+    }
+}
